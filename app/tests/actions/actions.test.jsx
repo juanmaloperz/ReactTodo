4e11641577e108d5f78976b4214f3495b3ff16d7
Juan Lopez <juanmaloperz@gmail.com>
2016-08-01 11:25:56 -0500

    addding actions to the app

---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,40 @@
+var expect = require('expect');
+var actions = require('actions');
+
+
+describe('Actions', ()=>{
+it('Shloud call the actions', () => {
+  var action = {
+    type: 'SET_SEARCH_TEXT',
+    searchText: 'Some Search Text'
+  }
+  var res = actions.setSearchText(action.searchText);
+  expect(res).toEqual(action);
+});
+it('Shloud call the actions ADD TODO', () => {
+  var action = {
+    type: 'ADD_TODO',
+    text: 'Something todo '
+  }
+  var res = actions.addTodo(action.text);
+  expect(res).toEqual(text);
+});
+it('Shloud call the actions TOGGLE Show Completed', () => {
+  var action = {
+    type: 'TOGGLE_SHOW_COMPLETED'
+
+  }
+  var res = actions.toggleShowCompleted();
+  expect(res).toEqual(action);
+});
+it('Shloud call the actions TOGGLE To do', () => {
+  var action = {
+    type: 'TOGGLE_TODO',
+    id
+  }
+  var res = actions.toggleTodo(action.id);
+  expect(res).toEqual(id);
+});
+
+
+});
